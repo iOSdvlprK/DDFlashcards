4f43f07c6ba04eee08da215fedf3ed044ea7921c
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-23 22:26:53 +0900

    Replace ContentView with TopicView as main view
    Removed ContentView and introduced TopicView as the new main view for the app. Updated DDFlashcardsApp to launch TopicView instead of ContentView.

--- DDFlashcards/DDFlashcardsApp.swift
+++ DDFlashcards/DDFlashcardsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DDFlashcardsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TopicView()
         }
     }
 }
---
+++ DDFlashcards/View/TopicView.swift
@@ -0,0 +1,24 @@
+//
+//  TopicView.swift
+//  DDFlashcards
+//
+//  Created by joe on 7/23/25.
+//
+
+import SwiftUI
+
+struct TopicView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("TODO...")
+            }
+            .navigationTitle("Flashcards")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TopicView()
+}

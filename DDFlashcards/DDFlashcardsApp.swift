d459ba97e5c3bc42368574649e88d7d0e999f12f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-23 22:04:16 +0900

    Initial Commit

---
+++ DDFlashcards/DDFlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDFlashcardsApp.swift
+//  DDFlashcards
+//
+//  Created by joe on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct DDFlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

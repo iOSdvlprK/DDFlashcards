e20454c76c94979f9a8caf941f85b61c45a0a24a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-31 21:48:07 +0900

    add TextAndOptionalImageView
    - introduces a reusable SwiftUI view that displays a text and, optionally, an image with optional 3D rotation
    - this component can be used to present questions and images in the app

---
+++ DDFlashcards/View/TextAndOptionalImageView.swift
@@ -0,0 +1,44 @@
+//
+//  TextAndOptionalImageView.swift
+//  DDFlashcards
+//
+//  Created by joe on 7/31/25.
+//
+
+import SwiftUI
+
+struct TextAndOptionalImageView: View {
+    let text: String
+    let imageName: ImageResource?
+    let rotate: Bool
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Text(text)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                if let imageName {
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxHeight: geometry.size.height * 0.7)
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+            }
+        }
+        .padding()
+        .rotation3DEffect(rotate ? .degrees(180) : .zero, axis: (x: 0.0, y: 1.0, z: 0.0))
+    }
+}
+
+#Preview {
+    TextAndOptionalImageView(
+        text: "What city is this?",
+        imageName: .bigBen,
+        rotate: false
+    )
+}
